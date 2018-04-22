f5b6ec52de75517db7b9079477cb77912149642a
Yingtao Tian <alan.yt.tian@gmail.com>
2018-04-22 18:42:57 -0400

    .

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 var debug = false;
 var debugURL = "http://pascal:8000/api/kanshi/sample";
-var productionURL = "https://alantian.net/api/kanshi/sample";
+var productionURL = "https://api.alantian.net/api/kanshi/sample";
 
 var Config = {
   url: debug? debugURL : productionURL
